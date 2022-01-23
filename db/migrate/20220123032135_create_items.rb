d1f5b675a7ea1b90e73b08e5be8655942ed98887
Rianna <riannaxcastro@gmail.com>
2022-01-22 17:45:39 -1000

    created tables

--- db/migrate/20220123032135_create_items.rb
+++ db/migrate/20220123032135_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
     create_table :items do |t|
       t.string :name
       t.string :price
+      t.belongs_to :category
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+men = Category.create(name: "Men")
+women = Category.create(name: "Women")
+kids = Category.create(name: "Kids")
+
+Item.create(name: "Original Black Tee", price: "$10.99", category: men);
+Item.create(name: "Distressed Jeans", price: "$49.99", category: men);
+Item.create(name: "Black Pocket Tee", price: "$14.99", category: men);
+Item.create(name: "Black Cargo Shorts", price: "$29.99", category: men);
+Item.create(name: "Black V-Neck Tee", price: "$11.99", category: women);
+Item.create(name: "High-Waisted Distressed Shorts", price: "$29.99", category: women);
+Item.create(name: "Original Boyfriend Tee", price: "$14.99", category: women);
+Item.create(name: "Black Super Slim Jeans", price: "$39.99", category: women);
+Item.create(name: "Basic Tee", price: "$11.99", category: kids);
+Item.create(name: "Original Longsleeve Shirt", price: "$14.99", category: kids);
+Item.create(name: "Unisex Shorts", price: "$10.99", category: kids);
+Item.create(name: "Cold Weather Jeans", price: "$18.99", category: kids);
+
+User.create(name: "Ri", username: "riri", password: "1111")
